0cf8ba2468b9704f8a8b75570a67297f837423e2
Melanie E Magdalena <archgirl@users.noreply.github.com>
2017-09-11 23:21:25 -0600

    added delete bookmark capability

--- js/bookmark.js
+++ js/bookmark.js
@@ -51,6 +51,28 @@ function saveBookmark(e){
   e.preventDefault();
 }
 
+// Delete bookmark
+function deleteBookmark(url){
+  
+  // Get bookmarks from localStorage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+  
+  // Loop throught bookmarks
+  for(var i =0;i < bookmarks.length;i++){
+    if(bookmarks[i].url == url){
+      // Remove from array
+      bookmarks.splice(i, 1);
+    }
+  }
+  
+  // Re-set back to localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fetch bookmarks
+  fetchBookmarks();
+  
+}
+
 // Fetch bookmarks
 function fetchBookmarks (){
   
